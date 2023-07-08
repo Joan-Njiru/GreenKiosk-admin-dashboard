6744e20bc10bf8bc49e38ae188270f828e880c2e
Your Name <joanwanjirunjiru@example.com>
2023-07-08 19:10:03 +0300

    django models

--- orders/models.py
+++ orders/models.py
@@ -1,17 +1,12 @@
 from django.db import models
-# from inventory.models import Product
 from payment.models import Payment
 from customer.models import Customer
-# from cart.models import Cart
 
 
 # Create your models here.
 class Orders(models.Model):
-    # products=models.ManyToManyField(Product)
     customer = models.ManyToManyField(Customer)
     payment = models.ManyToManyField(Payment)
-    # customer_order = models.ForeignKey(Cart,on_delete=models.CASCADE)
-    
     placement_date_time=models.DateTimeField()
     order_status=models.CharField(max_length=24)
     no_of_orders=models.IntegerField()
